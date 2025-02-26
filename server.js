22dd6a11fbd2ee499943cfb0c08b1417cb10b42d
Hiddensti <alrahmanihadi@gmail.com>
2025-02-26 13:26:12 +0300

    localhost working on 3001

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
 dotenv.config();
+
+require('./config/database');
 const express = require('express');
+
+// Auth
+const verifyToken = require('./middleware/verify-token');
+
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
 const app = express();
-const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
-// Routes go here
+// Routes
+app.use('/test-jwt', testJWTRouter); // REMOVE FOR TEST ONLY
+app.use('/users', usersRouter);
 
-app.listen(3000, () => {
+// Protected Routes
+app.use(verifyToken)
+app.use('/profiles', profilesRouter);
+
+app.listen(PORT, () => {
   console.log('The express app is ready!');
 });
